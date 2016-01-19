f5b1c34459868f8189b25954413049bc43fb64cc
joemccorkle <joe@joemccorkle.com>
2016-01-18 21:00:27 -0600

    Update config.js

--- app/config.js
+++ app/config.js
@@ -1,18 +1,18 @@
 module.exports = {
 	// Twilio API keys
 	twilio: {
-		sid: "ACCOUNT-SID",
-		token: "AUTH-TOKEN",
-		appid: 'APP-ID',
-		fromNumber : "TWILIO-NUMBER",
+		sid: "AC562edb02ad974e11dcecab7a40b95050",
+		token: "1e8b8a6abf253cf8f426513d7d9fc9ca",
+		appid: 'AP199da85a638fdb6c249e4fb8f4609402',
+		fromNumber : "+19723626973",
 		welcome : "Thank you for calling.",
 		hangup : false,
 		queueName: "cnacd"
 	},
 	//	Flybase settings
 	flybase: {
-		api_key: "YOUR-API-KEY",
-		app_name: "YOUR-FLYBASE-APP"
+		api_key: "87afa296-093d-4e5d-82bb-3f4bba3d301f",
+		app_name: "twiliodemo"
 	},
 	//	Username and password for admin section.
 	un: 'admin',
